bbf36b5f666b50e12afbb6c649f5d2639eeaa91d
Mwale09 <mwalekhololwethu@gmail.com>
2026-01-24 18:43:53 +0200

    c1

--- backend/livestock_tracking/urls.py
+++ backend/livestock_tracking/urls.py
@@ -7,10 +7,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls')),
-    path('tracking/', include('tracking.urls')),
-    path('notifications/', include('notifications.urls')),
+    path('/api/admin/', admin.site.urls),
+    path('/api/auth/', include('authentication.urls')),
+    path('/api/tracking/', include('tracking.urls')),
+    path('/api/notifications/', include('notifications.urls')),
 ]
 
 if settings.DEBUG:
